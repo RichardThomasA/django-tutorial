3d277ec73203202c60fb1d1484a795cfdeb4b45f
Richard Thomas <richard.t.alappat@gmail.com>
2015-11-04 16:14:27 +0530

    added a filter 'list_filter'

--- polls/admin.py
+++ polls/admin.py
@@ -17,5 +17,6 @@ class QuestionAdmin(admin.ModelAdmin):
 	]
 	inlines = [ChoiceInline]
 	list_display =('question_text','pub_date')
+	list_filter =['pub_date']
 
 admin.site.register(Question,QuestionAdmin)
